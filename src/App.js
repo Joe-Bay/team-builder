f377394d03fbba3bf47f1ed756bc14af2969dd00
Joe Bay <jbay55278@gmail.com>
2020-07-21 11:34:01 -0800

    added state and a good amount of app js done

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const initialFormValues = {
+  name: '',
+  hobbie: '',
+  country: '',
+  favoriteQuote: '',
+}
+
+function App() {
+  const [members, setMembers] = useState([])
+
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+
+const updateForm = (inputName, inputValue) => { // this is what makes it so our state will update when typing in the input
+  
+  setFormValues({ // this takes a spread of the values, and changes one depending on the values below which are usually event objects 
+    ...formValues,
+    [inputName]: inputValue,
+  })
+}
+
+const submitForm = () => {
+  const newMember = {// this is what creates a new member, the trim cuts any white space (spaces that someone may have inserted in the input box)
+    name: formValues.name.trim(),
+    hobbie: formValues.hobbie.trim(),
+    country: formValues.country.trim(),
+    favoriteQuote: formValues.favoriteQuote.trim(),
+  }
+  if(!newMember.name || newMember.hobbie || newMember.country || newMember.favoriteQuote) return
+
+  
+}
+
+
+
+  return (
+    <div className="App">
+
+
+    </div>
+  );
+}
+
+export default App;
